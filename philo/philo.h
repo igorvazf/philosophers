617e36816b713fdb453ba5bdff80172305794221
Igor Vaz Fernandes <igorv1286@gmail.com>
2022-03-12 23:37:36 -0300

    starting ... (?)

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igvaz-fe <igvaz-fe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 14:54:55 by igvaz-fe          #+#    #+#             */
+/*   Updated: 2022/03/12 14:55:41 by igvaz-fe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
